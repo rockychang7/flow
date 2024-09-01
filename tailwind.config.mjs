47b8a7f69e39541da91d39c983f562f95d0a9fb1
rockychang <1479762535@qq.com>
2024-09-01 15:49:01 +0800

    feat:add global font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import("tailwindcss").Config} */
 module.exports = {
     darkMode: ["class"],
@@ -51,6 +53,9 @@ module.exports = {
                 lg: "var(--radius)",
                 md: "calc(var(--radius) - 2px)",
                 sm: "calc(var(--radius) - 4px)",
+            },
+            fontFamily: {
+                sans: ["Roboto Mono Variable", "LXGWWenKaiMonoLite", ...defaultTheme.fontFamily.sans],
             },
             keyframes: {
                 "accordion-down": {
