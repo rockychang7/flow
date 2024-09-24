e72fb6fb7eb91539db78a30eea7189c762e0d72b
rockycheong <1479762535@qq.com>
2024-09-24 18:06:00 +0800

    feat:process common features

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import {defineConfig} from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind({
-        applyBaseStyles: false,
-    })]
+    integrations: [react(),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        mdx(),]
 });
